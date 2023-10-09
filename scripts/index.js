672ea40a67ba63b1ac5ef7df893434246cae0bd2
Emmanuel <oketunbi.olufunke@gmail.com>
2023-10-09 22:40:24 +0100

    about content added

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,47 @@ document.querySelectorAll(".bar-container").forEach((icon)=>{
       document.querySelector(".sm-pages-container").classList.toggle("show-nav")
   })
 })
+
+
+
+
+function toggleTabs(i){
+  const tabs = document.querySelectorAll(".tabs-content")
+  const tabButton = document.querySelectorAll(".tab-btn")
+
+  tabs.forEach(tab=>{
+    tab.style.display = "none"
+  })
+  tabs[i].style.display = "block"
+
+  tabButton.forEach(btn=>{
+    btn.style.color = "rgb(45, 44, 44)"
+  })
+  tabButton[i].style.color = "brown"
+
+
+}
+toggleTabs(0)
+
+
+let counter = 0
+const sliderImage = document.querySelectorAll(".slide-image")
+function sliderFn(i){
+  sliderImage.forEach(image=>{
+    image.style.left = "-200%";
+   document.documentElement.style.overflowX = "hidden"
+  })
+
+  sliderImage[i].style.left = `0%`
+  sliderImage[i].style.transition = "all 2s linear"
+}
+
+function IncreaseSlider() {
+  let newCount = counter >= sliderImage.length ? counter = 0 : counter++
+  sliderFn(newCount)
+}
+
+function DecreaseSlider() {
+  let newCount = counter <=0 ? counter = sliderImage.length-1 : counter--
+  sliderFn(newCount)
+}
